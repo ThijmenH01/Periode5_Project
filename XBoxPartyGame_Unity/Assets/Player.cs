8345887d3037c19b9b997b2c9aa17e1e5402c8a8
Thijmen <Thijmenh01@gmail.com>
2019-10-01 11:07:24 +0200

    Upload
    Upload

---
+++ XBoxPartyGame_Unity/Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 7.5f;
+    public float smoothMoveTime = 0.1f;
+    public float turnSpeed = 8;
+    float angle;
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
+    bool disabled;
+    Vector3 velocity;
+    Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        Vector3 _inputDirection = Vector3.zero;
+        if(!disabled) _inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        float _inputMagnitude = _inputDirection.magnitude;
+        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, _inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+
+        float _targetAngle = Mathf.Atan2(_inputDirection.x, _inputDirection.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle(angle, _targetAngle, Time.deltaTime * turnSpeed * _inputMagnitude);
+
+        velocity = transform.forward * moveSpeed * smoothInputMagnitude;
+    }
+
+    private void FixedUpdate() {
+        rb.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+        rb.MovePosition(rb.position + velocity * Time.deltaTime);
+    }
+}
