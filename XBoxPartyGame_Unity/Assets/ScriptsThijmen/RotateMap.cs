6f0f2edf74c28480c09560ba875c115a7c986e9b
Thijmen <Thijmenh01@gmail.com>
2019-10-08 09:43:36 +0200

    RotatingMap
    RotatingMap

---
+++ XBoxPartyGame_Unity/Assets/ScriptsThijmen/RotateMap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMap : MonoBehaviour {
+    [SerializeField] private float rotateSpeed;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        Rotate();
+    }
+
+    private void Rotate() {
+        transform.Rotate( 0 , rotateSpeed * Time.deltaTime , 0 );
+    }
+}
