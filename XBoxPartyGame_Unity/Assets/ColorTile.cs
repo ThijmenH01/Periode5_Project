e2285f6bdfc6823c477aeb757b6f5819981096cd
Thijmen <Thijmenh01@gmail.com>
2019-10-08 11:59:05 +0200

    tiles
    tiles

---
+++ XBoxPartyGame_Unity/Assets/ColorTile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorTile : MonoBehaviour {
+
+    public string color;
+    public string text;
+
+    void Start(){
+        
+    }
+
+    void Update(){
+        transform.rotation = Quaternion.Euler( 0 , 0 , 0 );
+    }
+}
--- XBoxPartyGame_Unity/Assets/ScriptsThijmen/Scripts/Player.cs
+++ XBoxPartyGame_Unity/Assets/ScriptsThijmen/Scripts/Player.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent( typeof( Rigidbody ) )]
 public class Player : MonoBehaviour {
-    
+
+    public enum TargetState {
+        color = 0,
+        text
+    }
+
+    [SerializeField] private TargetState targetState;
     [SerializeField] private float moveSpeed = 7.5f;
     [SerializeField] private float smoothMoveTime = 0.1f;
     [SerializeField] private float turnSpeed = 8;
@@ -15,39 +21,59 @@ public class Player : MonoBehaviour {
     private float smoothInputMagnitude;
     private float smoothMoveVelocity;
     private bool disabled;
-    private string vertical;
-    private string horizontal;
     private Vector2 moveInput;
+    private ColorTile colorTile;
 
-
-    private void FixedUpdate() 
-    {
-        rb.MoveRotation(Quaternion.Euler(Vector3.up * angle));
-        Move();
+    void Start() {
+        targetState = TargetState.color;
     }
 
-    public void OnMovement(InputAction.CallbackContext context)
-    {
+    private void Update() {
+        if(Input.GetKeyDown( KeyCode.LeftArrow )) targetState = TargetState.color;
+        if(Input.GetKeyDown(KeyCode.RightArrow)) targetState = TargetState.text;
+    }
+
+    #region Movement
+    private void FixedUpdate() {
+        rb.MoveRotation( Quaternion.Euler( Vector3.up * angle ) );
+        Move();
+        //transform.position = new Vector3( transform.position.x , Mathf.Sin( Time.time * 5 ) / 2 + 2 , transform.position.z );
+    }
+
+    public void OnMovement(InputAction.CallbackContext context) {
         moveInput = context.ReadValue<Vector2>();
     }
 
-    private void Move() 
-    {
-        if (!disabled)
-        {
-            Vector3 _inputDirection = new Vector3(moveInput.x, 0, moveInput.y);
+    private void Move() {
+        if(!disabled) {
+            Vector3 _inputDirection = new Vector3( moveInput.x , 0 , moveInput.y );
 
             float _inputMagnitude = _inputDirection.magnitude;
-            smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, _inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+            smoothInputMagnitude = Mathf.SmoothDamp( smoothInputMagnitude , _inputMagnitude , ref smoothMoveVelocity , smoothMoveTime );
 
-            float _targetAngle = Mathf.Atan2(_inputDirection.x, _inputDirection.z) * Mathf.Rad2Deg;
-            angle = Mathf.LerpAngle(angle, _targetAngle, Time.deltaTime * turnSpeed * _inputMagnitude);
+            float _targetAngle = Mathf.Atan2( _inputDirection.x , _inputDirection.z ) * Mathf.Rad2Deg;
+            angle = Mathf.LerpAngle( angle , _targetAngle , Time.deltaTime * turnSpeed * _inputMagnitude );
 
             Vector3 velocity = transform.forward * moveSpeed * smoothInputMagnitude;
-            rb.MovePosition(rb.position + velocity * Time.deltaTime);
+            rb.MovePosition( rb.position + velocity * Time.deltaTime );
+        }
+    }
+    #endregion
 
+    private void TargetStateBehavior() {
+        if(targetState == TargetState.color) {
+            print( "Actual Color: " + colorTile.color );
+        }
+
+        if(targetState == TargetState.text) {
+            print( "Text Color: " + colorTile.text );
         }
     }
 
-    
+    private void OnTriggerEnter(Collider col) {
+        if(col.gameObject.tag == "Tile") {
+            colorTile = col.GetComponent<ColorTile>();
+            TargetStateBehavior();
+        }
+    }
 }
