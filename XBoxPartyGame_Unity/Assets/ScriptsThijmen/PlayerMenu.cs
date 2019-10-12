cd93222109e07ba61a018f05353e68c75f27a532
Thijmen <Thijmenh01@gmail.com>
2019-10-12 14:28:19 +0200

    Basic Main Menu

---
+++ XBoxPartyGame_Unity/Assets/ScriptsThijmen/PlayerMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMenu : MonoBehaviour {
+
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}
